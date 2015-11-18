1cc92070e9946e0b21b535926548af29093b616c
gabe00122 <gabe.keith@yahoo.com>
2015-11-17 19:34:21 -0500

    Forget pacdot

---
+++ src/pacman/PacDot.java
@@ -0,0 +1,23 @@
+package pacman;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class PacDot extends Actor {
+	public PacDot(double x, double y) {
+		setSize(10, 10);
+		setPosition(x, y);
+	}
+	
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.gray);
+		g.fillOval((int)(getX()-getWidth()/2), (int)(getY()-getHeight()/2), (int)getWidth(), (int)getHeight());
+	}
+
+	@Override
+	public void update(double delta) {
+		
+	}
+
+}
